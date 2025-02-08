6bd7bf8f5c546946fca1753482ab6eb53cf2e495
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2025-02-08 12:00:56 -0800

    Add: tblShipping, ProductMaterial junction tbl.

--- yarn_it.sql
+++ yarn_it.sql
@@ -82,6 +82,56 @@ CREATE TABLE order_items_Table (
     subtotal DECIMAL(10,2) NOT NULL
 );
 
+CREATE TABLE tblProduct (
+	product_ID INT PRIMARY KEY AUTO_INCREMENT,
+    name_product VARCHAR(50) NOT NULL UNIQUE,
+    decsriptions VARCHAR(100),
+    price DECIMAL(10,2) NOT NULL,
+    category VARCHAR(25) NOT NULL,
+    size VARCHAR(25) NOT NULL,
+    color_main VARCHAR(25) NOT NULL,
+    weight DECIMAL(5,2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
+
+CREATE TABLE tblOrder_Items (
+	order_ID INT,
+	FOREIGN KEY(order_ID) REFERENCES tblOrder(order_ID),
+    product_ID INT,
+    FOREIGN KEY(product_ID) REFERENCES tblProduct(product_ID),
+    quantity INT NOT NULL,
+    subtotal DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE tblProduct_Material (
+	product_ID INT,
+	FOREIGN KEY(product_ID) REFERENCES tblproduct(product_ID),
+    material_ID INT,
+	FOREIGN KEY(material_ID) REFERENCES tblmaterial(material_ID),
+    quantity INT NOT NULL,
+    cost_per_unit DECIMAL(10,2) NOT NULL,
+    total_material_cost INT NOT NULL,
+    measurement_unit DECIMAL(5,2) NOT NULL,
+    wastage_percentage DECIMAL(5,2) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
 */
 
-SELECT * FROM tblProduct;
+CREATE TABLE tblShip (
+	shipping_ID INT PRIMARY KEY AUTO_INCREMENT,
+    order_ID INT,
+	FOREIGN KEY(order_ID) REFERENCES tblOrder(order_ID),
+    tracking_number VARCHAR(50) NOT NULL UNIQUE,
+    shipping_fee DECIMAL(10,2) NOT NULL,
+    shipping_method VARCHAR(50) NOT NULL,
+    shipping_address VARCHAR(100) NOT NULL,
+    estimated_delivery DATETIME NOT NULL, 
+    status_shipping VARCHAR(50) NOT NULL,
+    shipped_at DATETIME NOT NULL,
+    delivered_at DATETIME NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
